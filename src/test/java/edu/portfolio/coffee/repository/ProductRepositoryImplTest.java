333ce22e344c9d218c4ff6fe6dac0e5af4f45c22
adorahelen <adorahelenmin@gmail.com>
2024-09-01 20:36:53 +0900

    주석 추가, 가상 디비 사용->에러

--- src/test/java/edu/portfolio/coffee/repository/ProductRepositoryImplTest.java
+++ src/test/java/edu/portfolio/coffee/repository/ProductRepositoryImplTest.java
@@ -21,37 +21,45 @@ import static com.wix.mysql.distribution.Version.v8_0_11;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
-@SpringBootTest
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@ActiveProfiles("test")
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+
+@SpringBootTest  // Spring Boot 애플리케이션 컨텍스트를 로드하여 통합 테스트를 수행
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)  // 테스트 메서드의 실행 순서를 지정
+@ActiveProfiles("test")  // "test" 프로파일을 활성화하여 테스트 설정을 사용
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)  // 테스트 클래스의 생명주기를 클래스 단위로 설정
 class ProductJdbcRepositoryTest {
 
+    // 정적 필드로 EmbeddedMysql 인스턴스를 선언
     static EmbeddedMysql embeddedMysql;
 
     @BeforeAll
     static void setup() {
-        var config = aMysqldConfig(v8_0_11)
-                .withCharset(Charset.UTF8)
-                .withPort(2215)
-                .withUser("test", "test1234!")
-                .withTimeZone("Asia/Seoul")
+        // MySQL 서버를 구성
+        var config = aMysqldConfig(v8_0_11)  // MySQL 8.0.11 버전 사용
+                .withCharset(Charset.UTF8)  // UTF-8 문자셋 설정
+                .withPort(2215)  // MySQL 서버가 사용할 포트 설정
+                .withUser("test", "test1234!")  // 사용자 이름 및 비밀번호 설정
+                .withTimeZone("Asia/Seoul")  // 타임존 설정
                 .build();
+
+        // EmbeddedMysql 인스턴스를 생성하고 구성 설정을 적용
         embeddedMysql = anEmbeddedMysql(config)
-                .addSchema("test-order_mgmt", ScriptResolver.classPathScript("schema.sql"))
-                .start();
+                .addSchema("test-order_mgmt", ScriptResolver.classPathScript("schema.sql"))  // 데이터베이스 스키마를 스크립트 파일로부터 로드
+                .start();  // MySQL 서버 시작
     }
 
     @AfterAll
     static void cleanup() {
+        // 테스트 종료 후 EmbeddedMysql 서버를 정지
         embeddedMysql.stop();
     }
 
     @Autowired
-    ProductRepository repository;
+    ProductRepository repository;  // 테스트에 사용할 ProductRepository를 주입
 
+    // 테스트에 사용할 새로운 Product 객체 생성
     private final Product newProduct = new Product(UUID.randomUUID(), "new-product", Category.COFFEE_BEAN_PACKAGE, 1000L);
 
+    // 테스트 메서드들
     @Test
     @Order(1)
     @DisplayName("상품을 추가할 수 있다.")
